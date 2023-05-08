6d9dd44d80ef40e1480d3878316f3a0f17f857f0
svpaks <s.paksyuatkina@gmail.com>
2023-05-09 01:28:42 +0300

    Week2 Day2 exercises

---
+++ Week-2/Day2/DailyChallenge.py
@@ -0,0 +1,10 @@
+#Challenge 1
+
+number = int(input("Please, write a number."))
+length = int(input("Please, write a length."))
+lst_number = [number]
+for number1 in range(1, lst_number + 1) :
+    lst_number.append(lst_number(number1 - 1) * 2)
+
+
+#Challenge 2
