994a55a598ed16d2a755e7ae1049665adc0ef0a2
svpaks <s.paksyuatkina@gmail.com>
2023-07-09 14:55:16 +0300

    ExerciseXP

---
+++ Week-10/Day1/ExerciseXP/npm-node/main.js
@@ -0,0 +1,34 @@
+// Exercise 1 : Hello Node.Js
+
+// const a = 5;
+// const b = 10;
+// const sum = a + b;
+
+// console.log(`Hello you are ${sum}`);
+
+
+// Exercise 2 : Hello Node.Js #2
+// creating package.json file
+
+
+// Exercise 3 : Nodemon
+// Create a package.json file - use npm init
+// Install nodemon
+// Write the following code, in a main.js file:
+
+// Add some more code in the main.js, and run it using nodemon in order to log the sum of a + b.
+// Tip: you need to change the package.json file in order to achieve this.
+
+// Update the main.js file to console.log the sum of x + c
+
+const a = 5;
+const b = 10;
+let x;
+const c = 6;
+x = a + b;
+
+
+console.log("Sum of a+b:", x);
+console.log("Sum of x+c:", x+c);
+
+
